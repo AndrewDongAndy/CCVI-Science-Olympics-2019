7d888842388bf5b86391de368b19ed29273f8777
AndrewDongAndy <dongandrew99@gmail.com>
2019-01-11 13:01:54 -0500

    Generator expression

--- solutions/p1.py
+++ solutions/p1.py
@@ -1,9 +1,7 @@
 T = 10
 for _ in range(T):
     code = input()
-    s = 0
-    for i in code:
-        s += int(i)
+    s = sum(int(char) for char in code)
     if int(code[0]) >= 5 and s < 18:
         print("So cute!")
     else:
