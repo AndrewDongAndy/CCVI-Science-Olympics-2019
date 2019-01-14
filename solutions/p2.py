0c889f153e07411d7486beb5330f5ed24774f738
AndrewDongAndy <dongandrew99@gmail.com>
2019-01-14 14:39:51 -0500

    change N - K to -K

--- solutions/p2.py
+++ solutions/p2.py
@@ -6,4 +6,4 @@ for _ in range(10):
     N, K = [int(s) for s in input().split()]
     posts = [int(s) for s in input().split()]
     posts.sort();
-    print(sum(posts[:K]), sum(posts[N - K:]))
+    print(sum(posts[:K]), sum(posts[-K:]))
