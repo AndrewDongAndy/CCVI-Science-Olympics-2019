eeabf03ddef2822d089c40779353c47ad8adc765
AndrewDongAndy <dongandrew99@gmail.com>
2019-01-14 13:51:31 -0500

    Update p2_data.cpp

--- data/p2_data.cpp
+++ data/p2_data.cpp
@@ -1,4 +1,4 @@
-// January 10, 2019
+// January 14, 2019
 //
 
 /*
@@ -15,36 +15,32 @@ int rand_int(int a, int b) {
     return rand() % (b - a + 1) + a;
 }
 
+void print_case(int N, int K, int m) {
+    cout << N << ' ' << K << '\n';
+    for (int i = 0; i < N; i++) {
+        cout << rand_int(-m, m);
+        if (i != N - 1) cout << ' ';
+    }
+    cout << '\n';
+}
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
 
-#ifdef _DEBUG
+
     freopen("DATA22.txt", "w", stdout);
-#endif
 
 
     srand(time(0));
 
-    vector<int> sizes = {2, 5, 10, 20, 100, 1000, 10000, 100000, 100000, 100000};
-    for (int i = 0; i < 5; i++) {
-        int N = sizes[i], K = rand_int(0, N);
-        cout << N << ' ' << K << '\n';
-        for (int j = 0; j < N; j++) {
-            cout << rand_int(0, 1000);
-            if (j != N - 1) cout << ' ';
-        }
-        cout << '\n';
-    }
-    for (int i = 5; i < 10; i++) {
-        int N = sizes[i], K = rand_int(0, N);
-        cout << N << ' ' << K << '\n';
-        for (int j = 0; j < N; j++) {
-            cout << rand_int(-1000, 1000);
-            if (j != N - 1) cout << ' ';
-        }
-        cout << '\n';
+    vector<int> Ns = {2, 5, 10, 20, 100, 1000, 10000, 100000, 100000, 100000};
+    vector<int> ms = {5, 10, 30, 100, 200, 500, 1000, 1000, 1000, 1000};
+    for (int i = 0; i < 10; i++) {
+        int N = Ns[i], m = ms[i];
+        int K = (i == 2 ? 0 : rand_int(1, N));
+        print_case(N, K, m);
     }
 
 
