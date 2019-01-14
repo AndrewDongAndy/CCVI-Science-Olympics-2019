e1d6ffdef06a5566bc5073787887e4a77aebabd0
AndrewDongAndy <dongandrew99@gmail.com>
2019-01-14 14:21:14 -0500

    Add file input

--- solutions/p1.py
+++ solutions/p1.py
@@ -1,5 +1,9 @@
-T = 10
-for _ in range(T):
+f = open('DATA11.txt')
+
+def input():
+    return f.readline().strip()
+
+for _ in range(10):
     code = input()
     s = sum(int(char) for char in code)
     if int(code[0]) >= 5 and s < 18:
