19d98b07f394324a8ddc3a90dc9f71581fbfb107
AndrewDongAndy <dongandrew99@gmail.com>
2019-01-14 14:20:35 -0500

    Add file input

--- solutions/p2.py
+++ solutions/p2.py
@@ -1,5 +1,10 @@
+f = open('DATA21.txt')
+
+def input():
+    return f.readline().strip()
+
 for _ in range(10):
     N, K = [int(s) for s in input().split()]
     posts = [int(s) for s in input().split()]
     posts.sort();
-    print(sum(posts[0 : K]), sum(posts[N - K : N]))
+    print(sum(posts[:K]), sum(posts[N - K:]))
